22f1b73c149ce1b176f50917466c7ef20f779dab
akashcomputer04 <54447401+akashcomputer04@users.noreply.github.com>
2020-10-28 11:08:04 +0530

    del nf

--- New folder/reg1.py
+++
@@ -1,7 +0,0 @@
-import re
-st = "something is like someotherthing you know"
-m=re.match('some', st) # matches
-print(m)
-print(m.start())
-print(m.end())
-print(m.span())
--- New folder/reg2.py
+++
@@ -1,7 +0,0 @@
-import re
-st = "thing is like someotherthing you know"
-m=re.search('some', st) # matches
-print(m)
-print(m.start())
-print(m.end())
-print(m.span())
